0b361757246faf7696b168935430e1d372f99066
FLM210 <zflmiy@qq.com>
2021-11-21 12:23:50 +0800

    create

---
+++ Test/T1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "os"
+
+var a string
+
+func main() {
+	a = "G"
+	print(a)
+	f1()
+	fmt.Println("12")
+	os.
+}
+
+func f1() {
+	a := "O"
+	print(a)
+	f2()
+}
+
+func f2() {
+	print(a)
+}
